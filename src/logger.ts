1f9828474eaf9050f05459bf3c309da0033b3cb8
Andrey <andrishkin@gmail.com>
2024-06-27 16:01:31 +0500

    fix: fix logs

--- src/logger.ts
+++ src/logger.ts
@@ -3,10 +3,11 @@ import { createLogger, format, transports } from 'winston';
 const logger = createLogger({
     level: 'info', 
     format: format.combine(
-      format.timestamp(), 
+      format.timestamp(),
       format.printf(({ timestamp, level, message }) => {
         return `${timestamp} [${level}]: ${message}`;
-      })
+      }),
+      format.json()
     ),
     transports: [
       new transports.Console(), 
